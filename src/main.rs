2f2c235f2fca5ff7108800023e2586dfa314183e
Max Giga <gigamaximwachau@gmail.com>
2024-05-21 11:26:39 +0300

    close applications when the window was closed

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ mod canvas;
 
 use canvas::{Canvas, CanvasData};
 use druid::{
-    widget::{Button, Flex, Label},
-    AppLauncher, Env, Key, Selector, Widget, WidgetExt, WindowDesc,
+    widget::{Flex, Label},
+    AppDelegate, AppLauncher, Application, DelegateCtx, Env, Selector, Widget, WidgetExt,
+    WindowDesc, WindowId,
 };
 
 const GRAPH_COVERAGE: Selector = Selector::new("graph_coverage.compute_graph_coverage");
@@ -14,15 +15,13 @@ fn main() {
     let main_window = WindowDesc::new(ui_builder());
     let data = CanvasData::new();
     AppLauncher::with_window(main_window)
-        .log_to_console()
+        .delegate(Delegate)
         .launch(data)
         .expect("Failed to launch application");
 }
 
 fn ui_builder() -> impl Widget<CanvasData> {
     let canvas = Canvas::new();
-    let button = Button::new("Найти минимальное покрытие")
-        .on_click(|ctx, _data, _env| ctx.submit_command(GRAPH_COVERAGE));
     let label = Label::new(|data: &CanvasData, _env: &Env| {
         if data.coverage_error {
             "Невозможно найти минимальное покрытие"
@@ -32,10 +31,23 @@ fn ui_builder() -> impl Widget<CanvasData> {
     })
     .with_line_break_mode(druid::widget::LineBreaking::WordWrap);
     Flex::column()
-        .with_child(button)
-        .with_default_spacer()
         .with_child(label)
         .with_default_spacer()
         .with_child(canvas)
         .padding(10.0)
+}
+
+// correctly handle closing the window on macos
+struct Delegate;
+
+impl AppDelegate<CanvasData> for Delegate {
+    fn window_removed(
+        &mut self,
+        _id: WindowId,
+        _data: &mut CanvasData,
+        _env: &Env,
+        _ctx: &mut DelegateCtx,
+    ) {
+        Application::global().quit();
+    }
 }
